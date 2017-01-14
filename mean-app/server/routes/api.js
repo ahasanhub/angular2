d6735b9625016ea33c95a086c7a26f8bcaf247c0
Zayn An Nisa <shobuz_cse03@yahoo.com>
2017-01-14 21:01:24 +0600

    update from home

---
+++ mean-app/server/routes/api.js
@@ -0,0 +1,10 @@
+//Get dependencies
+const express=require('express');
+const router=express.Router();
+
+/* GET api listing */
+router.get('/',(req,res)=>{
+    res.send('api works');
+});
+
+module.exports=router;
